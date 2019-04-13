e14c70c6dacff05ec4bb088109a0e933aaf75ccd
Alex <alex.tervoort@gmail.com>
2019-04-13 11:55:49 +0200

    added colemak layer

--- keyboards/handwired/at75/keymaps/default/keymap.c
+++ keyboards/handwired/at75/keymaps/default/keymap.c
@@ -31,13 +31,13 @@ enum {
 
 // Layer Declarations
 enum {
-    _QW = 0,
-    _FN,
-    // ..., the rest of your layers
+    _QW = 0,//Querty default layer
+    _FN,	//Function layer
+	_CM		//Colemak layer
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* AT Copied from XD75 default QUERTY keymap */
+  [_QW] = LAYOUT( /* Copied from XD75 default QUERTY keymap */
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | GESC   | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | PR SCR |
@@ -59,6 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  KC_SPC,  LT(_FN, KC_SPC),  KC_RALT, KC_RGUI, KC_RCTL   \
   ),
   
+  [_FN] = LAYOUT( /* FUNCTION */
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
@@ -67,19 +68,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | PREV   | PLAY   | NEXT   | STOP   |        |        | P4     | P5     | P6     | P+     |        | RESET  |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    |        |        | P1     | P2     | P3     | PENT   |        |        |        |        | RSHIFT |
+ * | VOL-   | MUTE   | VOL+   | APP    |        |        | P1     | P2     | P3     | PENT   |        | _CM    |        |        | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        | FN     |        |        | P0     |        | P.     | PENT   |        | FN     |        |        | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
  
- [_FN] = LAYOUT( /* FUNCTION */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_PSLS, KC_PAST, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
     KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
     KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, RESET,   _______, _______, _______, \
-    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, KC_RSFT, \
+    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, TO(_CM), _______, _______, KC_RSFT, \
     _______, _______, _______, KC_TRNS, _______,_______, KC_P0,   _______, KC_PDOT, KC_PENT, _______, KC_TRNS, _______, _______, KC_RCTL  \
  ),
+ 
+  [_CM] = LAYOUT( /* Colemak */
+/* Colemak
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * | GESC   | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | _QW    |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * | TAB    | Q      | W      | F      | P      | G      | [      | \      | ]      | J      | L      | U      | Y      | ;      | '      |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
+ * | BACKSP | A      | R      | S      | T      | D      | HOME   | DEL    | PG UP  | H      | N      | E      | I      | O      | ENTER  |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
+ * | SFTCAP | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | K      | M      | ,      | .      | /      | RSHIFT |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+   KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TO(_QW),  \
+   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_LBRC, KC_BSLS, KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_QUOT,  \
+   KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_ENT,   \
+   TD(TD_LSFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
+   KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC,  KC_SPC,  LT(_FN, KC_SPC),  KC_RALT, KC_RGUI, KC_RCTL   \
+  ),
 };
 
 
