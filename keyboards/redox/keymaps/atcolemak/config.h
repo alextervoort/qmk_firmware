1db75185efac28511540ba05a166bbe80ee259fa
Alex <alex.tervoort@gmail.com>
2019-06-07 20:05:58 +0200

    Add split LED

--- keyboards/redox/keymaps/atcolemak/config.h
+++ keyboards/redox/keymaps/atcolemak/config.h
@@ -32,11 +32,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define USE_I2C
 
 /* Select hand configuration */
-//#define MASTER_LEFT
+#define SPLIT_HAND_PIN F4
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
 #undef RGBLED_NUM
+#define RGBLED_SPLIT { 7, 7 }
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 14
 #define RGBLIGHT_HUE_STEP 8
