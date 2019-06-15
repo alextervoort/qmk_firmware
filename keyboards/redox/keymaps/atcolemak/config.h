55ce84c729bf8973a21ed31d65426ae8920d6ff5
Alex <alex.tervoort@gmail.com>
2019-06-15 20:53:01 +0200

    Lots of changes due to troublshooting connection issues. Stable now.

--- keyboards/redox/keymaps/atcolemak/config.h
+++ keyboards/redox/keymaps/atcolemak/config.h
@@ -35,13 +35,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_HAND_PIN F4
 //#define MASTER_LEFT
 //#define MASTER_RIGHT
-//#define EE_HANDS
+//#define EE_HANDSxx`
 
+/* ws2812 RGB LED */
+#define RGB_DI_PIN D3
 #undef RGBLED_NUM
 #define RGBLED_NUM 14
 #define RGBLED_SPLIT { 7, 7 }
-//#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
+#define RGBLIGHT_LIMIT_VAL	175
 #define RGBLIGHT_SLEEP
+
+/* Audio */
+#define C6_AUDIO
+
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
+
+
