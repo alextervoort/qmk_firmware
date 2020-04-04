7cdc348d7bfbb4af81ed0b06dfbe63828f54f4ce
alextervoort <alex.tervoort@gmail.com>
2020-04-04 17:48:46 +0200

    update rules for bootloader and split config

--- keyboards/handwired/aso75/rules.mk
+++ keyboards/handwired/aso75/rules.mk
@@ -9,7 +9,7 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = atmel-dfu
+BOOTLOADER = qmk-dfu
 
 # Build Options
 #   change yes to no to disable
@@ -30,3 +30,5 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
+
+SPLIT_KEYBOARD = yes
