b19d43d86579cfa860b6b6423e20fddddf39ebca
alextervoort <alex.tervoort@gmail.com>
2020-07-22 20:20:11 +0200

    move arrow keys

--- keyboards/handwired/aso75/keymaps/default/keymap.c
+++ keyboards/handwired/aso75/keymaps/default/keymap.c
@@ -39,17 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
  * | BACKSP | A      | R      | S      | T      | D      | END    |   F3   |  |  PG DN | H      | N      | E      | I      | O      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
- * | SFTCAP | Z      | X      | C      | V      | B      | UP     |   F4   |  |  RIGHT | K      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | F5     |   F4   |  |   UP   | K      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   |   F5   |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | RIGHT  |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
  * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
  */
 
    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F1,   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME, KC_F2,   KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
    KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_END,  KC_F3,   KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP,   KC_F4,   KC_RGHT, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-   KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_F5,   KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F5,   KC_F4,   KC_UP,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+   KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_RGHT, KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL \
   ),
 
   [_QW] = LAYOUT( /* QWERTY keymap */
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
 * | F3     | BACKSP | A      | S      | D      | F      | G      | END    |  |  PG DN | H      | J      | K      | L      | :      | '      |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
-* | F4     | SFTCAP | Z      | X      | C      | V      | B      | UP     |  |  RIGHT | N      | M      | ,      | .      | /      | RSHIFT |
+* | F4     | LSHIFT | Z      | X      | C      | V      | B      | UP     |  |  RIGHT | N      | M      | ,      | .      | /      | RSHIFT |
 * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
 * | F5     | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
 * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
