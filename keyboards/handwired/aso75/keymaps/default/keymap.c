0e3ee925ec1b819d017e20604f55e4d8a6e7f67b
alextervoort <alex.tervoort@gmail.com>
2020-07-26 17:33:03 +0200

    keymap updates

--- keyboards/handwired/aso75/keymaps/default/keymap.c
+++ keyboards/handwired/aso75/keymaps/default/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
  * | GESC   | 1      | 2      | 3      | 4      | 5      | -      |   F1   |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | F      | P      | G      | HOME   |   F2   |  |  PG UP | J      | L      | U      | Y      | ;      | \      |
+ * | TAB    | Q      | W      | F      | P      | G      | HOME   |   [    |  |  PG UP | J      | L      | U      | Y      | ;      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
- * | BACKSP | A      | R      | S      | T      | D      | END    |   F3   |  |  PG DN | H      | N      | E      | I      | O      | '      |
+ * | BACKSP | A      | R      | S      | T      | D      | END    |   ]    |  |  PG DN | H      | N      | E      | I      | O      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | F5     |   F4   |  |   UP   | K      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F1,   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME, KC_F2,   KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
-   KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_END,  KC_F3,   KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
+   KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME, KC_LBRC, KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
+   KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_END,  KC_RBRC, KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F5,   KC_F4,   KC_UP,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_RGHT, KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL \
   ),
@@ -55,34 +55,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* QWERTY keymap */
 /* QWERTY
 * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
-* | F1     | GESC   | 1      | 2      | 3      | 4      | 5      | -      |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
+* | GESC   | 1      | 2      | 3      | 4      | 5      | -      |  F1    |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+-----------------|
-* | F2     | TAB    | Q      | W      | E      | R      | T      | HOME   |  |  PG UP | Y      | U      | I      | O      | P      | \      |
+* | TAB    | Q      | W      | E      | R      | T      | HOME   |  [     |  |  PG UP | Y      | U      | I      | O      | P      | \      |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
-* | F3     | BACKSP | A      | S      | D      | F      | G      | END    |  |  PG DN | H      | J      | K      | L      | :      | '      |
+* | BACKSP | A      | S      | D      | F      | G      | END    |  ]     |  |  PG DN | H      | J      | K      | L      | :      | '      |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
-* | F4     | LSHIFT | Z      | X      | C      | V      | B      | UP     |  |  RIGHT | N      | M      | ,      | .      | /      | RSHIFT |
+* | LSHIFT | Z      | X      | C      | V      | B      | F5     | F4     |  |  UP    | N      | M      | ,      | .      | /      | RSHIFT |
 * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
-* | F5     | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
+* | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | RIGHT  |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
 * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
 */
 
-   KC_F1,   KC_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,  \
-   KC_F3,   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,   \
-   KC_F4,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
-   KC_F5,   KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_DOWN, KC_SPC,  KC_SPC,  LT(_FN, KC_SPC),  KC_RALT, KC_RGUI, KC_RCTL   \
+   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F1,            KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_LBRC,          KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
+   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,  KC_RBRC,          KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,   \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F5,   KC_F4,            KC_UP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
+   KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_RGHT, KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL   \
   ),
 
 
   [_FN] = LAYOUT( /* FUNCTION */
 /* FUNCTION
  * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     |  | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
+ * |        | F1     | F2     | F3     | F4     | F5     | NUM LK | P/     |  | P*     | F6     | F7     | F8     | F9     | F10    | F11    |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
  * | SELECT | CALC   | MYCOMP | MAIL   |        |        | P7     | P8     |  | P9     | P-     |        |        | PR SCR | SCR LK | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   |        |        | P4     | P5     |  | P6     | P+     |        | RESET  |        |        |        |
+ * | PREV   | PLAY   | NEXT   | RESET  |        |        | P4     | P5     |  | P6     | P+     |        | RESET  |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
  * | VOL-   | MUTE   | VOL+   | APP    |        |        | P1     | P2     |  | P3     | PENT   |        | _CM    |        |        | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -90,11 +90,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
  */
 
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NLCK, KC_PSLS, KC_PAST, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, _______, KC_ASRP, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
-    KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, _______, KC_ASUP, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______, RESET,   _______, _______, _______, \
-    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  _______, KC_ASDN, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, TO(_CM), TO(_QW), _______, KC_RSFT, \
-    _______, _______, _______, KC_TRNS, _______, KC_ASTG, KC_P0,   _______, KC_PDOT, KC_PENT, _______, KC_TRNS, _______, _______, KC_RCTL  \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NLCK, KC_PSLS,     KC_PAST, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, _______, KC_ASRP, KC_P7,   KC_P8,       KC_P9,   KC_PMNS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
+    KC_MPRV, KC_MPLY, KC_MNXT, RESET,   _______, KC_ASUP, KC_P4,   KC_P5,       KC_P6,   KC_PPLS, _______, RESET,   _______, _______, _______, \
+    KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  _______, KC_ASDN, KC_P1,   KC_P2,       KC_P3,   KC_PENT, _______, TO(_CM), TO(_QW), _______, KC_RSFT, \
+    _______, _______, _______, KC_TRNS, _______, KC_ASTG, KC_P0,   _______,     KC_PDOT, KC_PENT, _______, KC_TRNS, _______, _______, KC_RCTL  \
  ),
 
 };
