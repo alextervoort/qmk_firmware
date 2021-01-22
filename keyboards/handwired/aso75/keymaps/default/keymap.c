5c376e2e79812717c7ca24b254165e252c40c930
alextervoort <alex.tervoort@gmail.com>
2021-01-22 17:27:41 +0100

    move f5, add copy-paste

--- keyboards/handwired/aso75/keymaps/default/keymap.c
+++ keyboards/handwired/aso75/keymaps/default/keymap.c
@@ -33,44 +33,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CM] = LAYOUT( /* Colemak */
 /* Colemak
  * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
- * | GESC   | 1      | 2      | 3      | 4      | 5      | -      |   F1   |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
+ * | GESC   | 1      | 2      | 3      | 4      | 5      | -      |   F5   |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | F      | P      | G      | HOME   |   [    |  |  PG UP | J      | L      | U      | Y      | ;      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
  * | BACKSP | A      | R      | S      | T      | D      | END    |   ]    |  |  PG DN | H      | N      | E      | I      | O      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | F5     |   F4   |  |   UP   | K      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | COPY   | PASTE  |  |   UP   | K      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
  * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | RIGHT  |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
  * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
  */
 
-   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F1,   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F5,   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_HOME, KC_LBRC, KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
    KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_END,  KC_RBRC, KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F5,   KC_F4,   KC_UP,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_COPY, KC_PSTE,   KC_UP,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_RGHT, KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL \
   ),
 
   [_QW] = LAYOUT( /* QWERTY keymap */
 /* QWERTY
 * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
-* | GESC   | 1      | 2      | 3      | 4      | 5      | -      |  F1    |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
+* | GESC   | 1      | 2      | 3      | 4      | 5      | -      |  F5    |  |  =     | 6      | 7      | 8      | 9      | 0      | DEL    |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+-----------------|
 * | TAB    | Q      | W      | E      | R      | T      | HOME   |  [     |  |  PG UP | Y      | U      | I      | O      | P      | \      |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+-----------------+--------|
 * | BACKSP | A      | S      | D      | F      | G      | END    |  ]     |  |  PG DN | H      | J      | K      | L      | :      | '      |
 * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------------------------+--------|
-* | LSHIFT | Z      | X      | C      | V      | B      | F5     | F4     |  |  UP    | N      | M      | ,      | .      | /      | RSHIFT |
+* | LSHIFT | Z      | X      | C      | V      | B      | COPY   | PASTE  |  |  UP    | N      | M      | ,      | .      | /      | RSHIFT |
 * |--------+--------+--------+--------+--------+-----------------+--------|  |--------+--------+--------+-----------------+--------+--------|
 * | LCTRL  | LGUI   | LALT   | FN     | SPACE  | SPACE  | LEFT   | RIGHT  |  |  DOWN  | ENTER  | ENTER  | FN     | RALT   | RGUI   | RCTRL  |
 * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
 */
 
-   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F1,            KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
+   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_F5,            KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_HOME, KC_LBRC,          KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
    KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_END,  KC_RBRC,          KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,   \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F5,   KC_F4,            KC_UP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_COPY, KC_PSTE,            KC_UP,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
    KC_LCTL, KC_LGUI, KC_LALT, LT(_FN, KC_SPC),  KC_SPC,  KC_SPC,  KC_LEFT, KC_RGHT, KC_DOWN, KC_ENT,  KC_ENT,  LT(_FN, KC_ENT),  KC_RALT, KC_RGUI, KC_RCTL   \
   ),
 
