033300cb3b7f5a7078688b19cc43546bc7e3621a
alextervoort <alex.tervoort@gmail.com>
2021-01-22 17:17:48 +0100

    added F12

--- keyboards/handwired/aso75/keymaps/default/keymap.c
+++ keyboards/handwired/aso75/keymaps/default/keymap.c
@@ -80,19 +80,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------------------------.  .--------------------------------------------------------------.
  * |        | F1     | F2     | F3     | F4     | F5     | NUM LK | P/     |  | P*     | F6     | F7     | F8     | F9     | F10    | F11    |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | CALC   | MYCOMP | MAIL   |        |        | P7     | P8     |  | P9     | P-     |        |        | PR SCR | SCR LK | PAUSE  |
+ * | SELECT | CALC   | MYCOMP | MAIL   |        |        | P7     | P8     |  | P9     | P-     |        |        | PR SCR | SCR LK | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | RESET  |        |        | P4     | P5     |  | P6     | P+     |        | RESET  |        |        |        |
+ * | PREV   | PLAY   | NEXT   | RESET  |        |        | P4     | P5     |  | P6     | P+     |        | RESET  |        |        | PAUSE  |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   | APP    |        |        | P1     | P2     |  | P3     | PENT   |        | _CM    |        |        | RSHIFT |
+ * | VOL-   | MUTE   | VOL+   | APP    |        |        | P1     | P2     |  | P3     | PENT   |        | _CM    | _QW    |        | RSHIFT |
  * |--------+--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        | FN     |        |        | P0     |        |  | P.     | PENT   |        | FN     |        |        | RCTRL  |
  * '-----------------------------------------------------------------------'  '--------------------------------------------------------------'
  */
 
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NLCK, KC_PSLS,     KC_PAST, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
-    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, _______, KC_ASRP, KC_P7,   KC_P8,       KC_P9,   KC_PMNS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, \
-    KC_MPRV, KC_MPLY, KC_MNXT, RESET,   _______, KC_ASUP, KC_P4,   KC_P5,       KC_P6,   KC_PPLS, _______, RESET,   _______, _______, _______, \
+    KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, _______, KC_ASRP, KC_P7,   KC_P8,       KC_P9,   KC_PMNS, _______, _______, KC_PSCR, KC_SLCK, KC_F12, \
+    KC_MPRV, KC_MPLY, KC_MNXT, RESET,   _______, KC_ASUP, KC_P4,   KC_P5,       KC_P6,   KC_PPLS, _______, RESET,   _______, _______, KC_PAUS, \
     KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  _______, KC_ASDN, KC_P1,   KC_P2,       KC_P3,   KC_PENT, _______, TO(_CM), TO(_QW), _______, KC_RSFT, \
     _______, _______, _______, KC_TRNS, _______, KC_ASTG, KC_P0,   _______,     KC_PDOT, KC_PENT, _______, KC_TRNS, _______, _______, KC_RCTL  \
  ),
